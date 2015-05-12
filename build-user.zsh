e35d65218a314b7470b1a95d848045694f50eaff
Liam Bowen <LiamBowen@gmail.com>
2015-05-12 15:21:11 -0400

    Make my account like I like it.

---
+++ build-user.zsh
@@ -0,0 +1,21 @@
+#!/usr/bin/zsh
+# Set up my regular user account
+cd
+
+# SSH Key
+ssh-keygen -t ecdsa -b 521 -N ''
+
+# Upload key to Github
+auth-json="$(printf '{"title":"%s","key":"%s"}\n' "cloud-$(date +%s)" "$(cat $HOME/.ssh/id_ecdsa.pub)")"
+curl -u 'hut8' --data "${auth-json}" 'https://api.github.com/user/keys'
+
+# Dotfiles except zsh
+git clone git@github.com:hut8/dotfiles
+./dotfiles/install
+
+# Prezto
+git clone --recursive git@github.com:hut8/prezto .zprezto
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
