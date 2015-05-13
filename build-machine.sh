aec51429879c7a3444ad25a2efd2b7b8994b352f
Liam Bowen <LiamBowen@gmail.com>
2015-05-13 18:30:56 -0400

    pv sudo locate, sudoers, ipython

--- build-machine.sh
+++ build-machine.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+# Debian 8 (jessie)
 
-# Rootly things
+# Packages
 apt-get update
 apt-get dist-upgrade
 apt-get install -y \
     cowsay git build-essential zsh htop iotop tmux rsync python-pip python3-pip \
     silversearcher-ag speedometer jq toilet wget curl apt-file pcregrep \
-    emacs-nox emacs-goodies-el
+    emacs-nox emacs-goodies-el pv sudo locate
 apt-file update
 pip install virtualenv
 pip install virtualenvwrapper
+pip install ipython
+pip3 install ipython
 
 # Make my account
 adduser liam --disabled-password --shell /usr/bin/zsh --gecos 'Liam'
@@ -18,3 +21,9 @@ cp /root/.ssh/authorized_keys /home/liam/.ssh/authorized_keys
 chown -R liam:liam /home/liam/.ssh
 touch /home/liam/.zshrc
 chown liam:liam /home/liam/.zshrc
+
+# Sudo
+echo 'liam ALL=(ALL:ALL) NOPASSWD: ALL' > /etc/sudoers.d/liam
+
+# Mail me
+# TODO
